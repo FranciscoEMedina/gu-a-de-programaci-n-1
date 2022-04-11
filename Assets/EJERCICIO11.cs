a2cd6d09bb7a12f488c76025543a5e9a1c7dce34
FranciscoEMedina <101721055+FranciscoEMedina@users.noreply.github.com>
2022-04-11 12:12:43 -0300

    commit til 13

---
+++ Assets/EJERCICIO11.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EJERCICIO11 : MonoBehaviour
+{
+    public int YYYYMMDD;
+    public string NombreComprador;
+    public string NombreProductor;
+    public int cantProducto;
+    public int precioProducto;
+    int total;
+    // Start is called before the first frame update
+    void Start()
+    {
+        total = cantProducto * precioProducto;
+        Debug.Log("Fecha de compra: " + YYYYMMDD);
+        Debug.Log("Nombre del comprador: " + NombreComprador);
+        Debug.Log("Producto solicitado: " + NombreProductor);
+        Debug.Log("Cantidad solicitada: " + cantProducto);
+        Debug.Log("Precio unitario: $" + precioProducto);
+        Debug.Log("Total a pagar: " + total);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/EJERCICIO12.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EJERCICIO12 : MonoBehaviour
+{
+    public int dado1;
+    public int dado2;
+    public int dado3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (dado1 >= 7 || dado2 >= 7 || dado3 >= 7)
+        {
+            Debug.Log("Los numeros que colocaste no son posibles en el dado que estamos utilizando");
+        }
+        else if (dado1 == 6 && dado2 == 6 && dado3 == 6)
+        {
+            Debug.Log("Exelente");
+        }
+
+        else if (dado1 == 6 && dado2 == 6 || dado1 == 6 && dado3 == 6|| dado3 == 6 && dado2 == 6)
+        {
+            Debug.Log("Muy bien");
+        }
+          else if (dado1 == 6 && dado3 < 6 && dado2 <6|| dado3 == 6 && dado1 < 6 && dado2 < 6 || dado2 == 6 && dado3 < 6 && dado1 < 6) 
+        {
+            Debug.Log("Regular");
+        }
+
+         else if (dado1 < 6 && dado2 < 6 && dado3 < 6)
+        {
+            Debug.Log("insuficiente");
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
